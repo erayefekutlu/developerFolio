a3f54ae039df1bbccc823a67358e28bbd755b837
Eray Efe Kutlu <erayefekutlu46@gmail.com>
2026-02-04 14:59:24 +0300

    Disable lottie animations for performance

--- src/components/displayLottie/DisplayLottie.js
+++ src/components/displayLottie/DisplayLottie.js
@@ -1,23 +1,5 @@
-import React, {Component, Suspense} from "react";
-import Lottie from "lottie-react";
-import Loading from "../../containers/loading/Loading";
+import React from "react";
 
-export default class DisplayLottie extends Component {
-  render() {
-    const animationData = this.props.animationData;
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData
-    };
-
-    return (
-      <Suspense fallback={<Loading />}>
-        <Lottie
-          animationData={defaultOptions.animationData}
-          loop={defaultOptions.loop}
-        />
-      </Suspense>
-    );
-  }
+export default function DisplayLottie() {
+  return null;
 }
